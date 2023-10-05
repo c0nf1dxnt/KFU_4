fe6f7f3ee834b1709412e81adbea516bf6ce6f23
c0nf1dxnt <c0nf1dxnt@yandex.ru>
2023-10-05 16:51:35 +0300

    4:51 PM 10/5/2023 version

--- homework30092023/Part1/Program.cs
+++ homework30092023/Part1/Program.cs
@@ -9,17 +9,46 @@ namespace Part1
         {
             return a > b ? a : b;
         }
-        static long Factorial(int a)
+        static void SwapValues(ref int a, ref int b)
+        {
+            (a, b) = (b, a);
+        }
+        static void SwapValues(ref decimal a, ref decimal b)
+        {
+            (a, b) = (b, a);
+        }
+        static bool CheckFactorial(int a, out long b)
+        {
+            try
+            {
+                b = 1;
+                for (int i = 2; i < a + 1; i++)
+                {
+                    b = checked(b * i);
+                }
+                return true;
+            }
+            catch (OverflowException)
+            {
+                b = 0;
+                return false;
+            }
+        }
+        static long FindFactorial(int a)
         {
             if (a == 0)
             {
                 return 1;
             }
-            return a * Factorial(a - 1);
+            return a * FindFactorial(a - 1);
         }
-        static int Fibonacchi(int a)
+        static long Fibonacchi(int a)
         {
-            if (a == 0 || a == 1)
+            if (a == 0)
+            {
+                return 0;
+            }
+            if (a == 1)
             {
                 return 1;
             }
@@ -51,9 +80,47 @@ namespace Part1
             string InputString1 = Console.ReadLine();
             Console.Write("Введите второе число: ");
             string InputString2 = Console.ReadLine();
-            if (Int32.TryParse(InputString1, out int number1) & Int32.TryParse(InputString2, out int number2))
+            if (Int32.TryParse(InputString1, out int     number1) & Int32.TryParse(InputString2, out int number2))
             {
                 Console.WriteLine($"Наибольшее число равно: {FindMax(number1, number2)}\n");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не целое число или строку!\n");
+            }
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Задание №2:\n");
+            Console.Write("Введите первое число: ");
+            string InputString1 = Console.ReadLine();
+            Console.Write("Введите второе число: ");
+            string InputString2 = Console.ReadLine();
+            if (Int32.TryParse(InputString1, out int number1) & Int32.TryParse(InputString2, out int number2))
+            {
+                SwapValues(ref number1, ref number2);
+                Console.WriteLine($"Первое число теперь равно: {number1}, а второе: {number2}\n");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не целое число или строку!\n");
+            }
+        }
+        static void Task3()
+        {
+            Console.WriteLine("Задание №3:\n");
+            Console.Write("Введите число: ");
+            string inputString = Console.ReadLine();
+            if (int.TryParse(inputString, out int number))
+            {
+                if (CheckFactorial(number, out long answer))
+                {
+                    Console.WriteLine($"Ответ: {number}! = {answer}\n");
+                }
+                else
+                {
+                    Console.WriteLine("В процессе вычисления возникло переполнение!\n");
+                }
             }
             else
             {
@@ -67,7 +134,7 @@ namespace Part1
             string InputString1 = Console.ReadLine();
             if (Int32.TryParse(InputString1, out int number))
             {
-                Console.WriteLine($"Ответ: {number}! = {Factorial(number)}\n");
+                Console.WriteLine($"Ответ: {number}! = {FindFactorial(number)}\n");
             }
             else
             {
@@ -77,14 +144,20 @@ namespace Part1
         static void Task5()
         {
             Console.WriteLine("Задание №5:\n");
-            Random random = new Random();
-            int number1 = random.Next(50);
-            int number2 = random.Next(50);
-            int number3 = random.Next(50);
-            Console.WriteLine(number1);
-            Console.WriteLine(number2);
-            Console.WriteLine(number3);
-            Console.WriteLine(GCD(number1,number2,number3) + "\n");
+            Console.Write("Введите первое число: ");
+            string InputString1 = Console.ReadLine();
+            Console.Write("Введите второе число: ");
+            string InputString2 = Console.ReadLine();
+            Console.Write("Введите третье число: ");
+            string InputString3 = Console.ReadLine();
+            if (Int32.TryParse(InputString1, out int number1) & Int32.TryParse(InputString2, out int number2) & (Int32.TryParse(InputString3, out int number3)))
+            {
+                Console.WriteLine($"НОД трёх введённых чисел равен: {GCD(number1,number2,number3)}\n");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не целое число или строку!\n");
+            }
         }
         static void Task6()
         {
@@ -107,6 +180,8 @@ namespace Part1
             Console.OutputEncoding = Encoding.Unicode;
 
             Task1();
+            Task2();
+            Task3();
             Task4();
             Task5();
             Task6();
--- homework30092023/Part2/Program.cs
+++ homework30092023/Part2/Program.cs
@@ -6,12 +6,17 @@ namespace Part2
     internal class Program
     {
 
+        static void Task1()
+        {
+            Console.WriteLine("Задание №1:\n");
+
+        }
         static void Main(string[] args)
         {
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
 
-
+            Task1();
 
             Console.ReadKey();
         }
